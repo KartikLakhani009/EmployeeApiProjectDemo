b8b4918c51c7d9f1cc6afd50cdf3bc035587ba28
Kartik Lakhani <kartik.lakhani@bacancytechnology.com>
2020-02-21 17:25:30 +0530

    Adding Redux

--- index.js
+++ index.js
@@ -6,5 +6,19 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import AppNavigator from './src/route/AppNavigator';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './src/Redux/index';
+import thunk from 'redux-thunk';
 
-AppRegistry.registerComponent(appName, () => AppNavigator);
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const AppContainer = () => {
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => AppContainer);
---
+++ src/Images/index.js
@@ -0,0 +1,6 @@
+export const AppImage = {
+  Motive: require('./Motive1.jpeg'),
+  Neurology: require('./neurology.jpg'),
+  Surgrey: require('./surgery.png'),
+  //   Tooth:require('./')
+};
